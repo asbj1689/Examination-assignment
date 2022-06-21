1eeac93a72e46436f710d8dbf09b27a655186071
asbj1689 <asbj1689@CV-BB-7513>
2022-06-21 14:58:56 +0200

    Last Update

---
+++ Examination program/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Examination_program
+{
+    class Repository
+    {
+            private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Camping database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    
+        public Repository()
+        {
+            SqlConnection connection = new(connectionString);
+            connection.Open();
+            connection.Close();
+        }
+    }
+}
