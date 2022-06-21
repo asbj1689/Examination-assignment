4e95d1949bbef64069bf6658213bd0efd9487855
asbj1689 <asbj1689@CV-BB-7513>
2022-06-21 15:00:12 +0200

    Change

--- Examination program/Repository.cs
+++ Examination program/Repository.cs
@@ -8,7 +8,6 @@ namespace Examination_program
             private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Camping database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     
         public Repository()
-            //Test//
         {
             SqlConnection connection = new(connectionString);
             connection.Open();
